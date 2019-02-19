a50478cf0e4acb7ce606d3c7d40b3f74b7be7737
Ayush <ayushs@mindfiresolutions.com>
2019-02-19 16:03:38 +0530

    update custom woocommere file for login authentication

--- themes/Avada/woocommerce/myaccount/form-login.php
+++ themes/Avada/woocommerce/myaccount/form-login.php
@@ -1,15 +1,8 @@
 <?php
-if(is_page('login')){
+if(is_page('client-login')){
     wc_get_template( 'myaccount/form-login-single.php' );
-} elseif(is_page('register')){
+} elseif(is_page('client-register')){
 	wc_get_template( 'myaccount/form-register-single.php' );
 } else{
-	wp_redirect('login');
-}
-
-
-if( isset($_GET['action']) == 'register' ) {
-	// die("register");	
-    wc_get_template( 'myaccount/form-register-single.php' );
-} else {
+	wp_redirect('client-login');
 }
