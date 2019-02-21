f85e973f3d83685c294623bdd0602c7cf829cbc4
Ayush <ayushs@mindfiresolutions.com>
2019-02-21 16:24:21 +0530

    Enable custom javascript for login and register page

--- themes/Avada-Child-Theme/assets/js/custom-script.js
+++ themes/Avada-Child-Theme/assets/js/custom-script.js
@@ -1,69 +1,69 @@
-// jQuery('form.woocommerce-form-login .woocommerce-Button')
-// .on('click', function(e){
-// 	debugger;
-// 	var usernameCheck = true;
-// 	var passwordCheck = true;
-// 	var form = jQuery(this).parents('form.woocommerce-form-login');
-// 	if(form.find('#username').val().length === 0){
-// 		usernameCheck = false;
-// 		form.find('#username').css('border-color', 'red');
-// 	} else{
-// 		usernameCheck = true;
-// 		form.find('#username').css('border-color', '#d6d6d6');
-// 	} 
-// 	if(form.find('#password').val().length === 0){
-// 		passwordCheck = false;
-// 		form.find('#password').css('border-color', 'red');
-// 	} else{
-// 		passwordCheck = true;
-// 		form.find('#password').css('border-color', '#d6d6d6');
-// 	}
-// 	if(!usernameCheck || !passwordCheck){
-// 		e.preventDefault();
-// 	} 
-// });
+jQuery('form.woocommerce-form-login .woocommerce-Button')
+.on('click', function(e){
+	debugger;
+	var usernameCheck = true;
+	var passwordCheck = true;
+	var form = jQuery(this).parents('form.woocommerce-form-login');
+	if(form.find('#username').val().length === 0){
+		usernameCheck = false;
+		form.find('#username').css('border-color', 'red');
+	} else{
+		usernameCheck = true;
+		form.find('#username').css('border-color', '#d6d6d6');
+	} 
+	if(form.find('#password').val().length === 0){
+		passwordCheck = false;
+		form.find('#password').css('border-color', 'red');
+	} else{
+		passwordCheck = true;
+		form.find('#password').css('border-color', '#d6d6d6');
+	}
+	if(!usernameCheck || !passwordCheck){
+		e.preventDefault();
+	} 
+});
 
 
-// jQuery('form.woocommerce-form-register .woocommerce-Button')
-// .on('click', function(e){
-// 	debugger;
-// 	var firstnameCheck = true;
-// 	var lastnameCheck = true;
-// 	var emailCheck = true;
-// 	var passwordCheck = true;
-// 	var form = jQuery(this).parents('form.woocommerce-form-register');
-// 	if(form.find('#reg_billing_first_name').val().length === 0){
-// 		firstnameCheck = false;
-// 		form.find('#reg_billing_first_name').css('border-color', 'red');
-// 	} else{
-// 		firstnameCheck = true;
-// 		form.find('#reg_billing_first_name').css('border-color', '#d6d6d6');
-// 	} 
-// 	if(form.find('#reg_billing_last_name').val().length === 0){
-// 		lastnameCheck = false;
-// 		form.find('#reg_billing_last_name').css('border-color', 'red');
-// 	} else{
-// 		lastnameCheck = true;
-// 		form.find('#reg_billing_last_name').css('border-color', '#d6d6d6');
-// 	}
-// 	if(form.find('#reg_email').val().length === 0){
-// 		emailCheck = false;
-// 		form.find('#reg_email').css('border-color', 'red');
-// 	} else{
-// 		emailCheck = true;
-// 		form.find('#reg_email').css('border-color', '#d6d6d6');
-// 	}
-// 	if(form.find('#reg_password').val().length === 0){
-// 		passwordCheck = false;
-// 		form.find('#reg_password').css('border-color', 'red');
-// 	} else{
-// 		passwordCheck = true;
-// 		form.find('#reg_password').css('border-color', '#d6d6d6');
-// 	}
-// 	if(!firstnameCheck || 
-// 		!lastnameCheck || 
-// 		!emailCheck || 
-// 		!passwordCheck){
-// 		e.preventDefault();
-// 	} 
-// });
+jQuery('form.woocommerce-form-register .woocommerce-Button')
+.on('click', function(e){
+	debugger;
+	var firstnameCheck = true;
+	var lastnameCheck = true;
+	var emailCheck = true;
+	var passwordCheck = true;
+	var form = jQuery(this).parents('form.woocommerce-form-register');
+	if(form.find('#reg_billing_first_name').val().length === 0){
+		firstnameCheck = false;
+		form.find('#reg_billing_first_name').css('border-color', 'red');
+	} else{
+		firstnameCheck = true;
+		form.find('#reg_billing_first_name').css('border-color', '#d6d6d6');
+	} 
+	if(form.find('#reg_billing_last_name').val().length === 0){
+		lastnameCheck = false;
+		form.find('#reg_billing_last_name').css('border-color', 'red');
+	} else{
+		lastnameCheck = true;
+		form.find('#reg_billing_last_name').css('border-color', '#d6d6d6');
+	}
+	if(form.find('#reg_email').val().length === 0){
+		emailCheck = false;
+		form.find('#reg_email').css('border-color', 'red');
+	} else{
+		emailCheck = true;
+		form.find('#reg_email').css('border-color', '#d6d6d6');
+	}
+	if(form.find('#reg_password').val().length === 0){
+		passwordCheck = false;
+		form.find('#reg_password').css('border-color', 'red');
+	} else{
+		passwordCheck = true;
+		form.find('#reg_password').css('border-color', '#d6d6d6');
+	}
+	if(!firstnameCheck || 
+		!lastnameCheck || 
+		!emailCheck || 
+		!passwordCheck){
+		e.preventDefault();
+	} 
+});
