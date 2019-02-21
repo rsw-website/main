8cb27dde2915387c67c20e3759914b894f617733
Ayush <ayushs@mindfiresolutions.com>
2019-02-21 15:40:16 +0530

    Add custom profile menu in header menu

---
+++ themes/Avada/includes/custom-menu.php
@@ -0,0 +1,33 @@
+<ul class="fusion-menu">
+	<?php
+	if(is_user_logged_in()){
+		$current_user = wp_get_current_user();
+		$menuitems = wp_get_nav_menu_items(17);
+	?>
+			<?php
+	    		$count = 0;
+	    		$submenu = false;
+			?>
+			<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children fusion-dropdown-menu fusion-last-menu-item">
+		        <a href="javascript:void(0)" class="fusion-bar-highlight">
+	    	    	<span class="menu-text">
+	    	    		<?php echo $current_user->display_name; ?>
+    	    		</span>
+	    		</a>
+	    		<ul role="menu" class="sub-menu fusion-switched-side">
+	    			<?php foreach( $menuitems as $item ): ?>
+		    			<li class="menu-item menu-item-type-custom menu-item-object-custom fusion-dropdown-submenu">
+					        <a href="<?php echo $item->url; ?>" class="fusion-bar-highlight">
+				    	    	<span><?php echo $item->title; ?></span>
+				    		</a>
+		    			</li>
+	    			<?php $count++; endforeach; ?>
+
+	    		</ul>
+			</li>   
+	<?php
+	} 
+	?>
+</ul>
+
+
