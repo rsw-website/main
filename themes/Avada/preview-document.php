7e6f0230a733a5bf67af16e3e9d9524b89aa58dd
Ayush <ayushs@mindfiresolutions.com>
2019-02-28 12:50:14 +0530

    Add new custom template for preview document

---
+++ themes/Avada/preview-document.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template Name: Preview Document
+ * Used for pages with a side-nav.
+ *
+ * @package Avada
+ * @subpackage Templates
+ */
+
+// Do not allow directly accessing this file.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'Direct script access denied.' );
+}
+?>
+
+<?php get_header(); ?>
+<h1>Preview document</h1>
+
+
