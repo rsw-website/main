a354a1c662e8d6742fe853e06733e3058de542b9
Ayush <ayushs@mindfiresolutions.com>
2019-03-07 16:40:53 +0530

    Update redict page code after registration

--- plugins/modify-admin-dashboard/plugin_configuration.php
+++ plugins/modify-admin-dashboard/plugin_configuration.php
@@ -113,10 +113,9 @@ function user_autologout(){
     if ( $approved_status == 'approved' ){
         return $redirect_url;
     } else{
-        echo add_query_arg( array( 'ref_id' => $user_id ),
-         get_permalink( get_page_by_path( 'registration-successful' )) );
-        // wp_logout();
-        // return get_permalink( get_page_by_path( 'client-registration' ) );
+        // echo add_query_arg( array( 'ref_id' => $user_id ), get_permalink( get_page_by_path( 'registration-successful' )) );
+        wp_logout();
+        return add_query_arg( array( 'ref_id' => base64_encode($user_id) ), get_permalink( get_page_by_path( 'registration-successful' )) )
     }
   }
 }
