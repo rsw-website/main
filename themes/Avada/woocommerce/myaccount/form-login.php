74e6e2497bb2e8e3c6d6906edb9a8f92c47cb2f3
Ayush <ayushs@mindfiresolutions.com>
2019-03-07 15:50:07 +0530

    update registration form text

--- themes/Avada/woocommerce/myaccount/form-login.php
+++ themes/Avada/woocommerce/myaccount/form-login.php
@@ -2,9 +2,6 @@
 if(is_page('client-login')){
     wc_get_template( 'myaccount/form-login-single.php' );
 } elseif(is_page('client-registration')){
-	?>
-	<p class="registration">Send in your registration application today!<br /> NOTE: Your account will be held for moderation and you will be unable to login until it is approved.</p>
-	<?php
 	wc_get_template( 'myaccount/form-register-single.php' );
 } else{
 	wp_redirect('client-login');
