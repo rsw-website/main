c0b09ccecc975dd653c58c06954c87e125ebf454
Ayush <ayushs@mindfiresolutions.com>
2019-03-11 12:00:49 +0530

    Add feature in client dashboard page

--- themes/Avada-Child-Theme/functions.php
+++ themes/Avada-Child-Theme/functions.php
@@ -34,3 +34,41 @@ function send_smtp_email( $phpmailer ) {
   $phpmailer->From       = 'sachdevaayush.sachdeva39@gmail.com';
   $phpmailer->FromName   = 'Reliable Softworks';
 }
+
+function my_account_menu_order() {
+  $menuOrder = array(
+    'edit-account'      => __( 'Account Details', 'woocommerce' ),
+    'my-document'          => __( 'My Documents', 'woocommerce' ),
+    'customer-logout'    => __( 'Logout', 'woocommerce' ),
+  );
+  return $menuOrder;
+ }
+ add_filter ( 'woocommerce_account_menu_items', 'my_account_menu_order' );
+
+ /*
+ * Step 2. Register Permalink Endpoint
+ */
+add_action('woocommerce_init', 'custom_add_endpoint');
+function custom_add_endpoint() {
+ 
+  // WP_Rewrite is my Achilles' heel, so please do not ask me for detailed explanation
+  add_rewrite_endpoint( 'my-document', EP_PAGES );
+}
+
+$endpoint = 'my-document';
+ 
+add_action( 'woocommerce_account_' . $endpoint .  '_endpoint', 'wk_endpoint_content' );
+ 
+function wk_endpoint_content() {
+    //content goes here
+    ?>
+      <h2 class="avada-woocommerce-myaccount-heading">
+        My Documents     
+      </h2>
+      <div class="document-request-text">
+        <p>You do not have permission to access the documents added by the admin.<br/>
+        Click on the below button to request access for all the documents.</p>
+        <button class="document-request-access">Request Document Access</button>
+      </div>
+    <?php 
+}
