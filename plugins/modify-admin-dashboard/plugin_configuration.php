3f4be1083db37d65e0efc2d9933937f2c440e3a6
Ayush <ayushs@mindfiresolutions.com>
2019-03-11 18:40:48 +0530

    Add column in dashboard section

--- plugins/modify-admin-dashboard/plugin_configuration.php
+++ plugins/modify-admin-dashboard/plugin_configuration.php
@@ -264,8 +264,7 @@ function custom_document_request_access($atts){
   if($accessStatus === 0){
     ?>
       <div class="document-request-text">
-        <p>You do not have permission to access the documents added by the admin.<br/>
-        Click on the below button to request access for all the documents.</p>
+        <p>You do not have permission to access the documents added by the admin.</p>
         <button class="document-request-access">Request Document Access</button>
         <div class="custom-loader lds-dual-ring hidden"></div>
       </div>
