ac26dd300df033b4669d37a70c2a9cc4b838db3b
Ayush <ayushs@mindfiresolutions.com>
2019-03-20 20:28:36 +0530

    update template file

--- themes/Avada/contact-us-template.php
+++ themes/Avada/contact-us-template.php
@@ -21,6 +21,94 @@ get_header(); ?>
 			<div class="post-content">
 				<?php the_content(); ?>
 			</div>
+			<form action="" method="post" class="avada-contact-form contact-form-details">
+					<?php if ( 'above' === Avada()->settings->get( 'contact_comment_position' ) ) : ?>
+					<?php endif; ?>
+					<div class="custom-form-row">
+						<div class="custom-form-col">
+							<label>Name<span class="required">*</span></label>
+							<div class="custom-form-holder">
+								<input type="text" name="contact_name" id="author" value="<?php echo esc_attr( $avada_contact->name ); ?>" size="22" required aria-required="true" aria-label="<?php esc_attr_e( 'Name (required)', 'Avada' ); ?>" class="custom-form-control input-name">
+							</div>
+						</div>
+						<div class="custom-form-col">
+							<label>Email<span class="required">*</span></label>
+							<div class="custom-form-holder">
+								<input type="email" name="email" id="email" value="<?php echo esc_attr( $avada_contact->email ); ?>" size="22" required aria-required="true" aria-label="<?php esc_attr_e( 'Email (required)', 'Avada' ); ?>" class="custom-form-control input-email">
+							</div>
+						</div>
+					</div>
+					<div class="custom-form-row">
+						<div class="custom-form-col">
+							<label>Phone<span class="required">*</span></label>
+							<div class="custom-form-holder">
+								<input type="text" name="url" id="url" value="<?php echo esc_attr( $avada_contact->phone ); ?>" aria-label="<?php esc_attr_e( 'Phone', 'Avada' ); ?>" size="22" class="custom-form-control input-website">
+							</div>
+						</div>
+						<div class="custom-form-col">
+							<label>Company<span class="required">*</span></label>
+							<div class="custom-form-holder">
+								<input type="text" name="company" id="company" value="<?php echo esc_attr( $avada_contact->company ); ?>" aria-label="<?php esc_attr_e( 'Company', 'Avada' ); ?>" size="22" class="custom-form-control input-website">
+							</div>
+						</div>
+					</div>
+					<div class="custom-form-row">
+						<div class="custom-form-col custom-form-area">
+							<label>Message</label>
+							<textarea name="msg" id="comment" cols="39" rows="4" class="" aria-label="<?php esc_attr_e( 'Message', 'Avada' ); ?>"><?php echo esc_textarea( $avada_contact->message ); // WPCS: CSRF ok. ?></textarea>
+						</div>
+					</div>
+					<div class="custom-form-row button-row">
+						<?php
+						global $fusion_settings;
+						if ( ! $fusion_settings ) {
+							$fusion_settings = Fusion_Settings::get_instance();
+						}
+
+						$button_shape = $fusion_settings->get( 'button_shape' );
+						$button_size  = $fusion_settings->get( 'button_size' );
+						$button_type  = $fusion_settings->get( 'button_type' );
+						?>
+						<?php if ( 'v3' === Avada()->settings->get( 'recaptcha_version' ) ) : ?>
+							<input type="hidden" name="fusion-recaptcha-response" id="fusion-recaptcha-response" value="">
+						<?php endif; ?>
+
+						<input name="submit" type="submit" id="submit" value="<?php esc_html_e( 'Submit', 'Avada' ); ?>" class="btn-c2 comment-submit fusion-button fusion-button-default fusion-button-default-size fusion-button-<?php echo esc_attr( strtolower( $button_size ) ); ?> fusion-button-<?php echo esc_attr( strtolower( $button_shape ) ); ?> fusion-button-<?php echo esc_attr( strtolower( $button_type ) ); ?>">
+					</div>
+					<div id="comment-input">
+						
+						
+						
+					</div>
+
+					<?php if ( 'above' !== Avada()->settings->get( 'contact_comment_position' ) ) : ?>
+						<div id="comment-textarea" class="fusion-contact-comment-below">
+							
+						</div>
+					<?php endif; ?>
+
+					<?php if ( Avada()->settings->get( 'contact_form_privacy_checkbox' ) ) : ?>
+						<div id="comment-privacy-checkbox-wrapper" class="fusion-comment-privacy-checkbox-wrapper">
+							<input type="checkbox" value="1" <?php checked( $avada_contact->data_privacy_confirmation, 1 ); ?> required aria-required="true" id="data-privacy-confirmation" name="data_privacy_confirmation" class="fusion-comment-privacy-checkbox" />
+							<label for="data-privacy-confirmation"><?php echo Avada()->settings->get( 'contact_form_privacy_label' ); // WPCS: XSS ok. ?></label>
+						</div>
+					<?php endif; ?>
+
+					<?php if ( Avada()->settings->get( 'recaptcha_public' ) && Avada()->settings->get( 'recaptcha_private' ) ) : ?>
+						<div id="comment-recaptcha">
+							<?php if ( 'v2' === Avada()->settings->get( 'recaptcha_version' ) ) : ?>
+								<div class="g-recaptcha" data-type="audio" data-theme="<?php echo esc_attr( Avada()->settings->get( 'recaptcha_color_scheme' ) ); ?>" data-sitekey="<?php echo esc_attr( Avada()->settings->get( 'recaptcha_public' ) ); ?>"></div>
+							<?php else : ?>
+								<?php $hide_badge_class   = 'hide' === Avada()->settings->get( 'recaptcha_badge_position' ) ? ' fusion-hide-recaptcha-badge' : ''; ?>
+								<div id="recaptcha-container" class="recaptcha-container<?php echo esc_attr( $hide_badge_class ); ?>"></div>
+							<?php endif; ?>
+						</div>
+					<?php endif; ?>
+
+					<div id="comment-submit-container">
+						
+					</div>
+				</form>
 		</div>
 	<?php endwhile; ?>
 </section>
