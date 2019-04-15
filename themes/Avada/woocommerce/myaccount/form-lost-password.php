4daace92534ad856e30ca5573341670695604c72
Ayush <ayushs@mindfiresolutions.com>
2019-04-15 16:15:23 +0530

    Remove username text from login and forget password page

--- themes/Avada/woocommerce/myaccount/form-lost-password.php
+++ themes/Avada/woocommerce/myaccount/form-lost-password.php
@@ -23,11 +23,11 @@ do_action( 'woocommerce_before_lost_password_form' );
 <form method="post" class="woocommerce-ResetPassword lost_reset_password">
 
 	<h2><?php echo apply_filters( 'woocommerce_lost_password_message', esc_html__( 'Lost your password', 'woocommerce' ) ); ?></h2>
-	<p><?php echo apply_filters( 'woocommerce_lost_password_message', esc_html__( 'Please enter your username or email address. You will receive a link to create a new password via email.', 'woocommerce' ) ); ?></p><?php // @codingStandardsIgnoreLine ?>
+	<p><?php echo apply_filters( 'woocommerce_lost_password_message', esc_html__( 'Please enter your email address. You will receive a link to create a new password via email.', 'woocommerce' ) ); ?></p><?php // @codingStandardsIgnoreLine ?>
 
 	<p class="woocommerce-form-row woocommerce-form-row--first form-row form-row-first">
-		<label for="user_login"><?php esc_html_e( 'Username or email', 'woocommerce' ); ?></label>
-		<input class="woocommerce-Input woocommerce-Input--text input-text" type="text" name="user_login" id="user_login" autocomplete="username" />
+		<label for="user_login"><?php esc_html_e( 'Email', 'woocommerce' ); ?></label>
+		<input class="woocommerce-Input woocommerce-Input--text input-text" type="email" name="user_login" id="user_login" autocomplete="username" />
 	</p>
 
 	<div class="clear"></div>
