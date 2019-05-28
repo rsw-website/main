417a6cda0754bb407932912bf5b9020056f2ded0
Ayush <ayushs@mindfiresolutions.com>
2019-05-28 13:52:44 +0530

    Add user ID address

--- plugins/loggedIn-user-activity/plugin_configuration.php
+++ plugins/loggedIn-user-activity/plugin_configuration.php
@@ -58,16 +58,15 @@ function user_activity_menu() {
 }
 
 function get_the_user_ip() {
-  if ( ! empty( $_SERVER['HTTP_CLIENT_IP'] ) ) {
-    //check ip from share internet
-    $ip = $_SERVER['HTTP_CLIENT_IP'];
-  } elseif ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
-    //to check ip is pass from proxy
-    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-  } else {
-    $ip = $_SERVER['REMOTE_ADDR'];
+  foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key) {
+     if (array_key_exists($key, $_SERVER) === true) {
+         foreach (explode(',', $_SERVER[$key]) as $ip) {
+            if (filter_var($ip, FILTER_VALIDATE_IP) !== false) {
+            return $ip;
+          }
+        }
+     }
   }
-  return $ip;
 }
 
 function get_user_activity(){
