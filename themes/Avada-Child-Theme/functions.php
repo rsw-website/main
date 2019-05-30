8fe56ee8e9022c20f9e99540139b16a8929bc74a
Ayush <ayushs@mindfiresolutions.com>
2019-05-30 21:58:08 +0530

    Update email template

--- themes/Avada-Child-Theme/functions.php
+++ themes/Avada-Child-Theme/functions.php
@@ -76,7 +76,7 @@ function custom_denied_notification_message($message, $user) {
 contact reach out to us.\r\n\r\n";
   $message .= "Sorry for any inconvenience.\r\n\r\n";
   $message .= "Best Regards,\n".get_option('blogname');
-  $message .= sprintf( esc_html__( 'Blog name is : %s'), '<strong>' . get_option('blogname') . '</strong>' ); 
+  $message .= <p>sprintf( esc_html__( "\nBlog name is : %s"), '<strong>' . get_option('blogname') . '</strong>' ); 
   return $message;
 }
 add_filter( 'new_user_approve_deny_user_message', 'custom_denied_notification_message', 10, 2 );
