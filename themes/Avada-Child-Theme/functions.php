aa4696de09fb2f8488785489d2dce196b663a703
Ayush <ayushs@mindfiresolutions.com>
2019-06-05 16:08:18 +0530

    Update default notification message content

--- themes/Avada-Child-Theme/functions.php
+++ themes/Avada-Child-Theme/functions.php
@@ -85,12 +85,12 @@ add_filter( 'new_user_approve_deny_user_message', 'custom_denied_notification_me
 */
 function custom_default_notification_message($message, $user) {
   $message = "<p>Hi Admin,</p>";
-  // $message .= "<p>$user->first_name $user->last_name has requested a username at ".get_option('blogname').".</p>";
-  // $message .= "<p>To approve or deny this user access to ".get_option('blogname')." go to ".admin_url()." </p>";
+  $message .= "<p>".$user->first_name." has requested a username at ".get_option('blogname').".</p>";
+  $message .= "<p>To approve or deny this user access to ".get_option('blogname')." go to ".admin_url('users.php?s&pw-status-query-submit=Filter&new_user_approve_filter=pending&paged=1')." </p>";
   $message .= "<p>Best Regards,<br>".get_option('blogname')."</p>";
   return $message;
 }
-add_filter( 'new_user_approve_request_approval_message_default', 'custom_default_notification_message', 10, 2 );
+add_filter( 'new_user_approve_request_approval_message', 'custom_default_notification_message', 10, 2 );
 
 function wpdocs_enqueue_custom_admin_script() {
          wp_enqueue_script( 'custom-script', plugins_url('documents-management/custom-script.js'), __FILE__);
